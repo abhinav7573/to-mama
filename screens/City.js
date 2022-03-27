093faefe4d5d0dbd92abfc508ed08911323b35a5
Abhinav coder <72642194+abhinav7573@users.noreply.github.com>
2022-03-27 20:27:12 +0530

    Add files via upload

---
+++ screens/City.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+
+export default class City extends Component {
+  render() {
+    return <Text> Cities</Text>;
+  }
+}
